db3adc309e0aa22dc542e36c1ad5fafe266d9ee1
Vĩ Tăng <tangtrieuvi@gmail.com>
2023-10-28 18:29:52 +0700

    declared the models for restaurant app and migrated to the MySQL database

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class booking(models.Model):
+    name = models.TextField(max_length=255)
+    no_of_guest = models.SmallIntegerField(default=0)
+    booking_date = models.DateTimeField(default=None)
+
+class menu(models.Model):
+    title = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField(default=0)
